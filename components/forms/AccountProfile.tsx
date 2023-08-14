ed62b9bf78888528b20bb109a1e8ec603df586be
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-14 18:44:24 +0700

    add: integrate uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -8,8 +8,10 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { UserValidation } from '@/lib/validations/user';
 import * as z from 'zod';
 import Image from 'next/image';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing';
 
 interface Props {
 	user: {
@@ -23,50 +25,63 @@ interface Props {
 	btnTitle: string;
 }
 
-const listInput = [
-	{
-		label: 'Name',
-		name: 'name',
-		type: 'text',
-	},
-	{
-		label: 'Username',
-		name: 'username',
-		type: 'text',
-	},
-	{
-		label: 'Bio',
-		name: 'bio',
-		type: 'textarea',
-	},
-];
-
 const AccountProfile = ({ user, btnTitle }: Props) => {
+	const [files, setFiles] = useState<File[]>([]);
+	const { startUpload } = useUploadThing('media');
+
 	const form = useForm({
 		defaultValues: {
-			profile_photo: '',
-			name: '',
-			username: '',
-			bio: '',
+			profile_photo: user?.image || '',
+			name: user?.name || '',
+			username: user?.username || '',
+			bio: user?.bio || '',
 		},
 		resolver: zodResolver(UserValidation),
 	});
 
-	const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+	const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
 		e.preventDefault();
+
+		const fileReader = new FileReader();
+
+		if (e.target.files && e.target.files.length > 0) {
+			const file = e.target.files[0];
+
+			setFiles(Array.from(e.target.files));
+
+			if (!file.type.includes('image')) return;
+
+			fileReader.onload = async (event) => {
+				const imageDataUrl = event?.target?.result?.toString() || '';
+
+				fieldChange(imageDataUrl);
+			};
+
+			fileReader.readAsDataURL(file);
+		}
 	};
 
-	function onSubmit(values: z.infer<typeof UserValidation>) {
+	async function onSubmit(values: z.infer<typeof UserValidation>) {
 		// Do something with the form values.
 		// ✅ This will be type-safe and validated.
-		console.log(values);
+		const blob = values.profile_photo;
+
+		const hasImageChanged = isBase64Image(blob);
+
+		if (hasImageChanged) {
+			const imgRes = await startUpload(files);
+
+			if (imgRes && imgRes[0].fileUrl) {
+				values.profile_photo = imgRes[0].fileUrl;
+			}
+		}
 	}
 
 	return (
 		<Form {...form}>
 			<form
-				onSubmit={form.handleSubmit(onSubmit)}
 				className='flex flex-col justify-start gap-10'
+				onSubmit={form.handleSubmit(onSubmit)}
 			>
 				<FormField
 					control={form.control}
@@ -77,7 +92,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 								{field.value ? (
 									<Image
 										src={field.value}
-										alt='profile photo'
+										alt='profile_icon'
 										width={96}
 										height={96}
 										priority
@@ -86,19 +101,18 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 								) : (
 									<Image
 										src='/assets/profile.svg'
-										alt='profile photo'
+										alt='profile_icon'
 										width={24}
 										height={24}
 										className='object-contain'
 									/>
 								)}
 							</FormLabel>
-
-							<FormControl className='flex-1 text-base-semibold text-gray-200 '>
+							<FormControl className='flex-1 text-base-semibold text-gray-200'>
 								<Input
 									type='file'
 									accept='image/*'
-									placeholder='Upload a photo'
+									placeholder='Add profile photo'
 									className='account-form_image-input'
 									onChange={(e) => handleImage(e, field.onChange)}
 								/>
@@ -107,39 +121,65 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 					)}
 				/>
 
-				{listInput.map((obj) => (
-					<FormField
-						key={obj.label}
-						control={form.control}
-						name={obj.name}
-						render={({ field }) => (
-							<FormItem className='flex flex-col gap-3 w-full'>
-								<FormLabel className='text-base-semibold text-light-2'>{obj.label}</FormLabel>
-								<FormControl>
-									{obj.type !== 'textarea' ? (
-										<Input
-											type='text'
-											className='account-form_input no-focus'
-											onChange={(e) => handleImage(e, field.onChange)}
-										/>
-									) : (
-										<Textarea
-											rows={10}
-											className='account-form_input no-focus'
-											{...field}
-										/>
-									)}
-								</FormControl>
-							</FormItem>
-						)}
-					/>
-				))}
+				<FormField
+					control={form.control}
+					name='name'
+					render={({ field }) => (
+						<FormItem className='flex w-full flex-col gap-3'>
+							<FormLabel className='text-base-semibold text-light-2'>Name</FormLabel>
+							<FormControl>
+								<Input
+									type='text'
+									className='account-form_input no-focus'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name='username'
+					render={({ field }) => (
+						<FormItem className='flex w-full flex-col gap-3'>
+							<FormLabel className='text-base-semibold text-light-2'>Username</FormLabel>
+							<FormControl>
+								<Input
+									type='text'
+									className='account-form_input no-focus'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name='bio'
+					render={({ field }) => (
+						<FormItem className='flex w-full flex-col gap-3'>
+							<FormLabel className='text-base-semibold text-light-2'>Bio</FormLabel>
+							<FormControl>
+								<Textarea
+									rows={10}
+									className='account-form_input no-focus'
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
 
 				<Button
 					type='submit'
 					className='bg-primary-500'
 				>
-					Submit
+					{btnTitle}
 				</Button>
 			</form>
 		</Form>
