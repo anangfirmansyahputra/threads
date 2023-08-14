8de69e868445fbe0e1808f4ef2a0160063dea133
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-14 20:22:02 +0700

    add: action, model user

---
+++ lib/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+	mongoose.set('strictQuery', true);
+
+	if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found!');
+	if (isConnected) return console.log('Alredy connected to MongoDB');
+
+	try {
+		await mongoose.connect(process.env.MONGODB_URL);
+
+		isConnected = true;
+	} catch (err) {
+		console.log(err);
+	}
+};
